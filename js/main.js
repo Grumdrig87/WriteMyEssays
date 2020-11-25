dcf0f316e0608657954830caf20fc1b23441a4f7
Grumdrig87 <70903272+Grumdrig87@users.noreply.github.com>
2020-11-25 14:51:03 +0300

    start

---
+++ js/main.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($){    
+
+
+    // // faq
+    // $('[data-faq]').click(function(){
+    //   $(this).find('.faq__title').toggleClass('open');
+    //   $(this).find('p').slideToggle(300);
+    // })
+    // burger
+
+    // $('[data-burger]').click(function(){
+    //   $('html').toggleClass("open");
+    //   $(this).toggleClass("open");
+    //   $('[data-nav]').toggleClass("open");
+    //   $('body').toggleClass('open');
+    // });
+})
