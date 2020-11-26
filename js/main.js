2f7abe47c9074decf8c48080d4ae463c72c07c80
Grumdrig87 <70903272+Grumdrig87@users.noreply.github.com>
2020-11-26 17:37:46 +0300

    f

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 jQuery(document).ready(function($){    
+    if (jQuery('[data-readmore]').length > 0) {
+      jQuery('[data-readmore]').readmore({
+        collapsedHeight: 313,
+        moreLink: '<a href="#">Read more</a>'
+      });
+    }
+    
     $('[data-nav] > ul >li').hover(function(){
         $(this).each(function(){
           $('[data-nav] li').toggleClass('transp');
         });
         $(this).removeClass('transp');
-      })
-
+      });
+        //tabs
+      $('[data-can]').on('click', function() {
+          $(this).addClass('active').siblings().removeClass('active')
+            .closest('.can__wrap').find('ul.can__list').removeClass('active').eq($(this).index()).addClass('active');
+          }); 
     // // faq
     // $('[data-faq]').click(function(){
     //   $(this).find('.faq__title').toggleClass('open');
