e23bad342b840f7d92f65921c0b1d61356aa8b27
Вит ноут <grumdrig@mail.ru>
2020-11-30 00:12:40 +0300

    1

--- js/main.js
+++ js/main.js
@@ -5,6 +5,18 @@ jQuery(document).ready(function($){
         moreLink: '<a href="#">Read more</a>'
       });
     }
+    if (jQuery('[data-readmoref]').length > 0) {
+      jQuery('[data-readmoref]').readmore({
+        collapsedHeight: 189,
+        moreLink: '<a href="#">Read more</a>'
+      });
+    }
+    if (jQuery('[data-readmoret]').length > 0) {
+      jQuery('[data-readmoret]').readmore({
+        collapsedHeight: 380,
+        moreLink: '<a href="#">Read more</a>'
+      });
+    }
     
     $('[data-nav] > ul >li').hover(function(){
         $(this).each(function(){
@@ -17,11 +29,36 @@ jQuery(document).ready(function($){
           $(this).addClass('active').siblings().removeClass('active')
             .closest('.can__wrap').find('ul.can__list').removeClass('active').eq($(this).index()).addClass('active');
           }); 
-    // // faq
-    // $('[data-faq]').click(function(){
-    //   $(this).find('.faq__title').toggleClass('open');
-    //   $(this).find('p').slideToggle(300);
-    // })
+    // team
+    $('[data-team]').click(function(){
+      $(this).toggleClass('open');
+      $(this).find('.team__title').toggleClass('open');
+      $(this).find('p').slideToggle(300);
+    })
+    //slider
+    if (jQuery('[data-writers]').length > 0) {
+      $('[data-writers]').slick({
+        dots: true,
+        speed: 300,
+        slidesToShow: 2,
+        // responsive: [
+        // {
+        //     breakpoint: 1200,
+        //     settings: {
+        //     slidesToShow: 2
+        //     }
+        // },
+        // ]
+    });
+    }
+   
+    // faq
+    if (jQuery('[data-faq]').length > 0) {
+      $('[data-faq]').click(function(){
+        $(this).find('.faq__title').toggleClass('open');
+        $(this).find('p').slideToggle(300);
+      })
+    }
     // burger
 
     // $('[data-burger]').click(function(){
